dd2ac74eb03be17ce86c9a632d11bf603b18e7da
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-07 21:32:31 +0900

    Update worker-common dependency, implement NFT ownership synchronization, and configure cron triggers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { syncNftOwnership } from "@gaiaprotocol/worker-common";
+import { createPublicClient, http } from "viem";
+import { mainnet } from "viem/chains";
 import { handleGetNames } from "./handlers/get-names";
 import { handleNotices } from "./handlers/notice";
 import { handleSearchNames } from "./handlers/search-names";
 import { handleSetName } from "./handlers/set-name";
 import { preflightResponse } from "./services/cors";
+
+const client = createPublicClient({ chain: mainnet, transport: http() })
 
 export default {
   async fetch(request, env, ctx): Promise<Response> {
@@ -18,5 +23,14 @@ export default {
     if (url.pathname === '/search-names') return handleSearchNames(request, env);
 
     return new Response('Not Found', { status: 404 });
+  },
+
+  async scheduled(controller, env, ctx) {
+    await syncNftOwnership(env, client, {
+      "0x134590ACB661Da2B318BcdE6b39eF5cF8208E372": {
+        start: 0,
+        end: 3332
+      }
+    }, 1000);
   },
 } satisfies ExportedHandler<Env>;
