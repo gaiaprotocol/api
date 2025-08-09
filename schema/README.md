ecf2544bc8c9504cbf3ff745bba6c33e37948c76
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-09 13:37:15 +0900

    feat: add SQL script for inserting NFT test data

--- schema/README.md
+++ schema/README.md
@@ -8,4 +8,6 @@ wrangler d1 execute gaiaprotocol --local --file=./schema/notices_insert.sql
 wrangler d1 execute gaiaprotocol --local --file=./schema/gaia_names.sql
 wrangler d1 execute gaiaprotocol --local --file=./schema/god_metadata.sql
 wrangler d1 execute gaiaprotocol --local --file=./schema/supabase_dump.sql
+
+wrangler d1 execute gaiaprotocol --local --file=./schema/insert_nft_test.sql
 ```
---
+++ schema/insert_nft_test.sql
@@ -0,0 +1,17 @@
+INSERT INTO nfts (
+  nft_address,
+  token_id,
+  holder,
+  type,
+  gender,
+  parts,
+  image
+) VALUES (
+  '0x134590ACB661Da2B318BcdE6b39eF5cF8208E372',
+  1,
+  '0xbB22b6F3CE72A5Beb3CC400d9b6AF808A18E0D4c',
+  'Stone',
+  'Man',
+  '{"Body":"None","Face":"Teenager","Hair":"Curly","Hand":"None","Background":"Background","Front Accessory":"None","Background Accessory":"Sun"}',
+  '2973/00fafb91-d2fd-4b23-bc01-1d484da208c3.png'
+);
