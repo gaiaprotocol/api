65fe89f6614d169e85d49c3da712693a267faa42
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-14 13:18:28 +0900

    chore: update devDependencies and regenerate worker configuration types

---
+++ src/services/god-image.ts
@@ -0,0 +1,7 @@
+import { getSelectedParts, GodMetadata } from '@gaiaprotocol/god-mode-shared';
+
+export async function generateGodImage(env: Env, url: string, metadata: GodMetadata) {
+  const selectedParts = getSelectedParts(metadata);
+
+  //TODO
+}
