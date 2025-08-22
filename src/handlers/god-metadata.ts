37b958d017b7d65acfa7f244f4fe38c909df9b3a
yj.gaia <gaiaprotocolnft@gmail.com>
2025-08-22 21:51:14 +0900

    feat: add animation URL to NFT metadata in handleGodMetadata function

--- src/handlers/god-metadata.ts
+++ src/handlers/god-metadata.ts
@@ -32,6 +32,7 @@ export async function handleGodMetadata(
 
     const metadata = metadataTransformer.toOpenSeaFormat(nft);
     metadata.description = `A membership NFT collection of Gaia Protocol consisting of 3,333 NFTs\n\nDownload Image: https://god-images.gaia.cc/${tokenId}/${nft.image}`;
+    metadata.animation_url = `https://gods.gaia.cc/god-viewer/${tokenId}`;
 
     return jsonWithCors(metadata);
   } catch (err) {
