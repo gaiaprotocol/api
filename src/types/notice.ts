dc1022af85339cad57cd99b0ab122a66724903c2
yj.gaia <gaiaprotocolnft@gmail.com>
2025-09-19 14:10:32 +0900

    fix: update translations type in Notice interface to support nested records

--- src/types/notice.ts
+++ src/types/notice.ts
@@ -3,5 +3,5 @@ export interface Notice {
   title: string;
   content: string;
   createdAt: number;
-  translations?: Record<string, string>;
+  translations?: Record<string, Record<string, string>>;
 }
